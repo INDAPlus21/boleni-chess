b0a0338b140c5c6ab1f1a69bf33461679f85c96b
Jonatan Bölenius <boleni@kth.se>
2021-09-25 15:26:03 +0200

    Chose format for field and initiated it

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::option::Option;
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum GameState {
@@ -7,13 +8,37 @@ pub enum GameState {
     GameOver
 }
 
-/* IMPORTANT:
- * - Document well!
- * - Write well structured and clean code!
- */
+
+// Color enum
+#[derive(Copy, Clone)]
+pub enum Color {
+    White,
+    Black
+}
+
+
+// Piece enum
+#[derive(Copy, Clone)]
+pub enum Piece {
+    King,
+    Queen,
+    Bishop,
+    Knight,
+    Rook,
+    Pawn
+}
+
+impl Piece {
+    fn take_turn(&self, /*...*/)  {
+        
+    }
+
+    // ...
+}
 
 pub struct Game {
     /* save board, active colour, ... */
+    board: [[Option<(Piece, Color)>; 8]; 8],
     state: GameState
 }
 
@@ -22,9 +47,37 @@ impl Game {
     pub fn new() -> Game {
         Game {
             /* initialise board, set active colour to white, ... */
-            state: GameState::InProgress
+
+            state: GameState::InProgress,
+            board: {
+                let mut board: [[Option<(Piece, Color)>; 8]; 8] = [[None; 8]; 8];
+                for (row, row_pieces) in board.iter_mut().enumerate() {
+                    for (column, piece) in row_pieces.iter_mut().enumerate() {
+                        let color = {if row == 0 {Color::Black} else {Color::White}};
+                        match row {
+                            0 | 7 => {
+                                *piece = {
+                                    match column {
+                                        0 | 7 => {Some((Piece::Rook, color))},
+                                        1 | 6 => {Some((Piece::Knight, color))},
+                                        2 | 5 => {Some((Piece::Bishop, color))},
+                                        3 => {Some((Piece::Queen, color))},
+                                        4 => {Some((Piece::King, color))},
+                                        _ => {None}
+                                    }
+                                }
+                            }
+                            1 | 6 => { *piece = Some((Piece::Pawn, color)); }
+                            _ => {continue;}
+                        }
+                    }
+                }
+                board
+            }
         }
     }
+
+    
 
     /// If the current game state is InProgress and the move is legal, 
     /// move a piece and return the resulting state of the game.
@@ -32,7 +85,7 @@ impl Game {
         None
     }
 
-    /// Set the piece type that a peasant becames following a promotion.
+    /// Set the piece type that a peasant becomes following a promotion.
     pub fn set_promotion(&mut self, _piece: String) -> () {
         ()
     }
@@ -68,7 +121,7 @@ impl fmt::Debug for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         /* build board representation string */
         
-        write!(f, "")
+        write!(f, "s");
     }
 }
 
